2453bfafde72ce02454baae1b4b03212c30b6f82
swhyeon98 <smreo75@gmail.com>
2023-07-15 18:09:27 +0900

    Initial commit

---
+++ src/main/java/com/example/mustache_with_springboot_test/MustacheWithSpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mustache_with_springboot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheWithSpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheWithSpringbootTestApplication.class, args);
+	}
+
+}
